0804e317ea4257a0fc514f95bec052c34a392b9b
monte <greg.jonca@gmail.com>
2018-03-31 21:22:34 +0200

    Created first basic component: product-list component

---
+++ src/app/products/product-list.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'pm-products',
+    templateUrl: './product-list.component.html'
+})
+export class ProductListComponent {
+    pageTitle: 'Product List';
+}
