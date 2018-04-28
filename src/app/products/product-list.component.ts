ef89be4209a0d5bc3a135ff77007960aaf8039d7
monte <greg.jonca@gmail.com>
2018-04-28 15:36:10 +0200

    Add first service via DI

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import { IProduct } from './product';
+import { ProductService } from '../../api/products/product.service';
 @Component({
     // tslint:disable-next-line:component-selector
     selector: 'pm-products',
@@ -24,33 +25,10 @@ export class ProductListComponent implements OnInit {
     }
 
     filteredProducts: IProduct[];
-    products: IProduct[] = [
-        {
-            // tslint:disable:quotemark
-            "productId": 1,
-            "productName": "Leaf Rake",
-            "productCode": "GDN-0011",
-            "releaseDate": "March 19, 2016",
-            "description": "Leaf rake with 48-inch wooden handle.",
-            "price": 19.95,
-            "starRating": 2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/26215/Anonymous_Leaf_Rake.png"
-        },
-        {
-            "productId": 2,
-            "productName": "Garden Cart",
-            "productCode": "GDN-0023",
-            "releaseDate": "March 18, 2016",
-            "description": "15 gallon capacity rolling garden cart",
-            "price": 32.99,
-            "starRating": 4.2,
-            "imageUrl": "http://openclipart.org/image/300px/svg_to_png/58471/garden_cart.png"
-        }
-    ];
-
-    constructor() {
-        this.filteredProducts = this.products;
-        this.listFilter = 'cart';
+    products: IProduct[];
+    _productService: ProductService;
+    constructor(service: ProductService) {
+        this._productService = service;
     }
 
     onRatingClicked(message: string): void {
@@ -69,5 +47,8 @@ export class ProductListComponent implements OnInit {
 
     ngOnInit(): void {
         console.log('On Init');
+        this.products = this._productService.getProducts();
+        this.filteredProducts = this.products;
+        this.listFilter = 'cart';
     }
 }
