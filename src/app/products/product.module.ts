67a8d8f39a29e643053068cbc543e03e38006696
monte <greg.jonca@gmail.com>
2018-05-03 10:53:37 +0200

    Add shared module

--- src/app/products/product.module.ts
+++ src/app/products/product.module.ts
@@ -3,28 +3,25 @@ import { CommonModule } from '@angular/common';
 import { ProductListComponent } from './product-list.component';
 import { ProductDetailComponent } from './product-detail.component';
 import { ConvertToSpaces } from '../shared/convert-to-spaces.pipe';
-import { StarComponent } from '../shared/star.component';
-import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
 import { ProductGuard } from './product-guard.service';
 import { ProductService } from '../../api/products/product.service';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
   imports: [
-    CommonModule,
-    FormsModule,
     RouterModule.forChild([
       { path: 'products', component: ProductListComponent },
       { path: 'products/:id',
         canActivate: [ ProductGuard ],
         component: ProductDetailComponent }
-    ])
+    ]),
+    SharedModule
   ],
   declarations: [
     ProductListComponent,
     ProductDetailComponent,
     ConvertToSpaces,
-    StarComponent
   ],
   providers: [
     ProductService,
